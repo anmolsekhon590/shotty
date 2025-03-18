8fe6c271084e617de5193247818861f9ae70f0c0
Anmol Sekhon <19473358-anmolsekhon@users.noreply.gitlab.com>
2025-03-18 13:12:51 -0400

    formatted

--- src/commands/args.rs
+++ src/commands/args.rs
@@ -2,7 +2,11 @@ use clap::Parser;
 
 /// Shotty: A simple Wayland screenshot wrapper
 #[derive(Parser)]
-#[command(version, about = "A simple Wayland screenshot wrapper", long_about = "A wayland screenshot wrapper for grim and slurp. Requires compositor to support the unstable wayland screencopy protocol")]
+#[command(
+    version,
+    about = "A simple Wayland screenshot wrapper",
+    long_about = "A wayland screenshot wrapper for grim and slurp. Requires compositor to support the unstable wayland screencopy protocol"
+)]
 pub struct Args {
     /// Take a fullscreen screenshot
     #[arg(short = 'f', long = "fullscreen")]
